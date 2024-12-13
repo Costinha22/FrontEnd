b1ed17a9e9faf8874220d5c0f187ca913f2ad827
Costinha22 <joaocosta.fed@gmail.com>
2024-12-13 16:37:38 +0000

    ediçao web

---
+++ Algoritmia/JavaScript/Exercicios/FichaTrabalho01/Exercicio01.js
@@ -0,0 +1,19 @@
+//Exercicio 1
+let frase = "Aprender Java é fixe!"
+frase = frase.length
+console.log(frase)
+
+//Exercicio 2
+let string = "javascript é demais!";
+
+console.log(string[0]);
+console.log(string[10]);
+
+//Exercicio3
+let texto = "a Lais é a melhor formadora do mundo"
+
+let texto1 = texto.toUpperCase()
+let texto2 = texto.toLowerCase()
+
+console.log(texto1)
+console.log(texto2)
---
+++ Algoritmia/src/FichaTrabalho06/Exercicio05.java
@@ -0,0 +1,34 @@
+package FichaTrabalho06;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+    public static void main(String[] args) throws FileNotFoundException  {
+
+        //Indicar o caminho do ficheiro
+        File file = new File("FrontEnd/Algoritmia/ficheiro06/exercicio_05_31.txt");
+
+
+        //Ler o ficheiro
+        Scanner scanner = new Scanner(file);
+
+
+        //Guardar o conteudo numa variavel
+        int soma = 0;
+
+
+
+        //Ciclo para somar
+        while(scanner.hasNextInt()){
+            soma += scanner.nextInt();
+        }
+        System.out.println("A soma dos números é: " +soma);
+
+
+
+    }
+
+}
