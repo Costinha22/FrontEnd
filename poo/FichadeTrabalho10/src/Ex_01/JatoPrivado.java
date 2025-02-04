1b5bb4a0ec277a09e7dd5095046d181737d2e693
Costinha22 <joaocosta.fed@gmail.com>
2025-02-04 12:54:54 +0000

    java POO

---
+++ poo/FichadeTrabalho10/src/Ex_01/JatoPrivado.java
@@ -0,0 +1,12 @@
+package Ex_01;
+
+public class JatoPrivado extends Aviao {
+
+
+    private int lotacaoPassageiros;
+    private double capacidadeBagagem;
+    private String categoria;
+    private String instalacoes;
+
+
+}
